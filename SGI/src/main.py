ca79ab477e84eddb89707bd08a18604ac7d92e91
Arthur Crippa <tucamilanez@hotmail.com>
2022-04-22 14:27:40 -0300

    Add Scroll Area

--- SGI/src/main.py
+++ SGI/src/main.py
@@ -6,11 +6,32 @@ def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("teste")
-    
-    label = QtWidgets.QLabel(win)
+    win.setWindowTitle("SGI")
+
+    button1 = QtWidgets.QPushButton(win)
+    button1.setText("1")
+    button1.setGeometry(0,0, 50, 30)
+
+    button2 = QtWidgets.QPushButton(win)
+    button2.setText("2")
+    button2.setGeometry(0, 30, 50, 30)
+
+    button3 = QtWidgets.QPushButton(win)
+    button3.setText("3")
+    button3.setGeometry(0, 60, 50, 30)
+
+    button4 = QtWidgets.QPushButton(win)
+    button4.setText("4")
+    button4.setGeometry(0, 90, 50, 30)
+
+    vp = QtWidgets.QAbstractScrollArea(win)
+    vp.setGeometry(60, 0, 300, 300)
+
+    label = QtWidgets.QLabel(vp)
     label.setText("teste2")
     label.move(50, 50)
+
+
 
     win.show()
     sys.exit(app.exec_())
