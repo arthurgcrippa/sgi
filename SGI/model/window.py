43feb4aa830ab7d63ba95d6b2374a93c10aa7e96
Tiago Possenti <tiagosefirotmon@tutanota.com>
2022-06-17 13:25:34 -0300

    Window last changes

--- SGI/model/window.py
+++ SGI/model/window.py
@@ -28,31 +28,31 @@ class Window():
     def move(self, direction: int):
         diff = MOVE
         if direction == 1:
-            self.yMin = self.yMin + diff
-            self.yMax = self.yMax + diff
+            self.yMin += diff
+            self.yMax += diff
         elif direction == 2:
-            self.yMin = self.yMin - diff
-            self.yMax = self.yMax - diff
+            self.yMin -= diff
+            self.yMax -= diff
         elif direction == 3:
-            self.xMin = self.xMin - diff
-            self.xMax = self.xMax - diff
+            self.xMin -= diff
+            self.xMax -= diff
         elif direction == 4:
-            self.xMin = self.xMin + diff
-            self.xMax = self.xMax + diff
+            self.xMin += diff
+            self.xMax += diff
         elif direction == 5:
-            self.zMin = self.zMin + diff
-            self.zMax = self.zMax + diff
+            self.zMin = self.zMin += diff
+            self.zMax = self.zMax += diff
         elif direction == 6:
-            self.zMin = self.zMin - diff
-            self.zMax = self.zMax - diff
+            self.zMin = self.zMin -= diff
+            self.zMax = self.zMax -= diff
 
     def zoom(self, sign):
         zoomX = self.vp_width  * sign*ZOOM
         zoomY = self.vp_height * sign*ZOOM
         zoomZ = self.vp_depth  * sign*ZOOM
-        self.xMin = self.xMin + zoomX
-        self.xMax = self.xMax - zoomX
-        self.yMin = self.yMin + zoomY
-        self.yMax = self.yMax - zoomY
-        self.zMin = self.zMin + zoomZ
-        self.zMax = self.zMax - zoomZ
+        self.xMin += zoomX
+        self.xMax -= zoomX
+        self.yMin += zoomY
+        self.yMax -= zoomY
+        self.zMin = self.zMin += zoomZ
+        self.zMax = self.zMax -= zoomZ
