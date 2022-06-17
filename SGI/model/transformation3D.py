Trigonometric logic fixed, Rotation quasi-operational

SGI/model/transformation3D.py
@@ -1,6 +1,7 @@
 from model.form import Form
 from model.transformation import Transformation
 from typing import Tuple, List
+from utils import matrices
 import numpy as np
 import math
 
@@ -28,117 +29,56 @@ class Transformation3D(Transformation):
 
     def translation(self):
         x, y, z = self.point[0], self.point[1], self.point[2]
-        self.matrix = [[1,0,0,0],
-                       [0,1,0,0],
-                       [0,0,1,0],
-                       [x,y,z,1]]
+        self.matrix = matrices.translation((x,y,z),matrices.identity())
 
     def rotation(self):
         p1, p2 = (0,0,0), self.point
         x, y, z = self.object.get_center()
-        p2 = (p2[0]-x, p2[1]-y, p2[2]-z)
-
-        #1 Transladar para a origem
-        matrix_translation = [[1,0,0,0],
-                              [0,1,0,0],
-                              [0,0,1,0],
-                              [-x,-y,-z,1]]
-
+        m = matrices.translation((-x,-y,-z),[p2[0],p2[1],p2[2],1])
+        p2 = (m[0],m[1],m[2])
+        matrix = matrices.identity()
+        #1 Transladar para a Origem
+        matrix1 = matrices.translation((-x,-y,-z),matrix)
         #2 Rotacionar sobre o eixo-x
         p3 = (p2[0], p2[1], 0)
-        sin, cos = self.trigonometry(p1,p2,p3)
-        sin_x, cos_x = sin,cos
-        matrix_rotation_x = [[1,   0,    0,    0],
-                             [0,  cos,  sin,   0],
-                             [0, -sin,  cos,   0],
-                             [0,   0,    0,    1]]
-
+        theta_x = matrices.degree(p1,p2,p3)
+        matrix2 = matrices.rotation_x(theta_x, matrix1)
         #3 Rotacionar sobre o eixo-z
-        p3 = (0, p2[1], p2[2])
-        sin, cos = self.trigonometry(p1,p2,p3)
-        sin_z, cos_z = sin,cos
-        matrix_rotation_z = [[cos, sin,   0,  0],
-                             [-sin, cos,  0,  0],
-                             [0,     0,   1,  0],
-                             [0,     0,   0,  1]]
-
-        #4 Rotacionar sobre o eixo-y em tetha
-        tetha = math.radians(self.degree)
-        sin, cos = np.sin(tetha), np.cos(tetha)
-        matrix_rotation_y = [[cos, 0, -sin, 0],
-                             [0,   1,   0,  0],
-                             [sin, 0,  cos,  0],
-                             [0,   0,   0,  1]]
-
-        #4 Desfazer rotação sobre o eixo-z
-        sin, cos = -sin_z, -cos_z
-        matrix_reverse_rotation_z = [[cos, sin,   0,  0],
-                                     [-sin, cos,  0,  0],
-                                     [0,     0,   1,  0],
-                                     [0,     0,   0,  1]]
-
-        #5 Desfazer rotação sobre o eixo-x
-        sin, cos = -sin_x, -cos_x
-        matrix_reverse_rotation_x = [[1,   0,    0,    0],
-                                     [0,  cos,  sin,   0],
-                                     [0, -sin,  cos,   0],
-                                     [0,   0,    0,    1]]
-
-        #Desfazer a translação
-        matrix_reverse_translation = [[1,0,0,0],
-                                      [0,1,0,0],
-                                      [0,0,1,0],
-                                      [x,y,z,1]]
-        self.matrix = np.dot(np.dot(np.dot(np.dot(np.dot(np.dot(matrix_translation, matrix_rotation_x),matrix_rotation_z),matrix_rotation_y),matrix_reverse_rotation_z),matrix_reverse_rotation_x),matrix_reverse_translation)
+        m = matrices.rotation_x(theta_x,[p2[0],p2[1],p2[2],1])
+        p2 = (m[0],m[1],m[2])
+        p3 = (0, p2[1], 0)
+        theta_z = matrices.degree(p1,p2,p3)
+        matrix3 = matrices.rotation_z(theta_z, matrix2)
+        #4 Rotacionar sobre o eixo-y
+        theta = self.degree
+        matrix4 = matrices.rotation_y(theta, matrix3)
+        #5 Desfazer Rotação sobre o eixo-z
+        matrix5 = matrices.rotation_z(-theta_z, matrix4)
+        #6 Desfazer Rotação sobre o eixo-x
+        matrix6 = matrices.rotation_x(-theta_x, matrix5)
+        #7 Desfazer Translação
+        matrix7 = matrices.translation((x,y,z), matrix6)
+        self.matrix = matrix7
 
     def scaling(self):
-        dx, dy, dz = self.point[0], self.point[1], self.point[2]
-        x, y, z = self.object.get_center()
-
-        matrix_scaling = [[dy,0,0,0],
-                          [0,dy,0,0],
-                          [0,0,dz,0],
-                          [0,0,0,1]]
-
-        matrix_translation = [[1,   0,  0, 0],
-                              [0,   1,  0, 0],
-                              [0,   0,  1, 0],
-                              [-x, -y, -z, 1]]
-
-        matrix_reverse_translation = [[1,0,0,0],
-                                      [0,1,0,0],
-                                      [0,0,1,0],
-                                      [x,y,z,1]]
-
-        self.matrix = np.dot(np.dot(matrix_translation, matrix_scaling), matrix_reverse_translation)
-
+        scale = self.point
+        matrix = matrices.identity()
+        center = self.object.get_center()
+        self.matrix = matrices.scaling(scale, center, matrix)
 
     def window_rotation(self):
-
-        tetha = math.radians(self.degree)
-        sin, cos = np.sin(tetha), np.cos(tetha)
+        theta = self.degree
         x, y, z = self.point
-
-        matrix_window_rotation = [[cos, sin,   0,  0],
-                                  [-sin, cos,  0,  0],
-                                  [0,     0,   1,  0],
-                                  [0,     0,   0,  1]]
-
-        matrix_translation = [[1,   0,  0, 0],
-                              [0,   1,  0, 0],
-                              [0,   0,  1, 0],
-                              [-x, -y, -z, 1]]
-
-        matrix_reverse_translation = [[1,0,0,0],
-                                      [0,1,0,0],
-                                      [0,0,1,0],
-                                      [x,y,z,1]]
-
-        self.matrix = np.dot(np.dot(matrix_window_rotation, matrix_translation), matrix_reverse_translation)
+        matrix = matrices.identity()
+        matrix = matrices.translation((-x,-y,-z), matrix)
+        matrix = matrices.rotation_z(theta, matrix)
+        matrix = matrices.translation((x,y,z), matrix)
+        self.matrix = matrix
 
 
     def apply(self):
         self.object.setMatrix(np.dot(self.object.matrix, self.matrix))
+        #self.object.setMatrix(self.matrix)
         self.object.reform()
 
     def normalize(self):
@@ -148,13 +88,26 @@ class Transformation3D(Transformation):
             x, y, z = line[0], line[1], line[2]
             self.object.normalized.append([x,y,z])
 
-    def trigonometry(self, p1, p2, p3):
+    def degree(self, p1, p2, p3):
         hipo = self.dist(p1,p2)
         cat_op = self.dist(p2,p3)
         cat_ad = self.dist(p1, p3)
+        sign, x, y = 1, p3[0], p3[1]
         sin = cat_op/hipo
         cos = cat_ad/hipo
-        return sin, cos
+        quad = 0
+        degree = np.degrees(np.arcsin(sin))
+        if x < 0:
+            if y < 0: #3rd
+                degree = 180 - degree
+            else:     #2nd
+                degree += 180
+        else:
+            if y < 0: #4th
+                degree += 360
+            else:     #1st
+                pass
+        return degree
 
     def dist(self, p1, p2):
         return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2 + (p2[2]-p1[2])**2)